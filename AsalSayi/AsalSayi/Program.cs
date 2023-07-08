375e9620fdf038570db28a048cf2b7b77cdac78e
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-08 21:49:56 +0300

    AsalSayi

---
+++ AsalSayi/AsalSayi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AsalSayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            Console.Write("Kontrol etmek istediğiniz sayıyı Girin :");
+            int sayi = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    sayac++;
+                }
+            }
+            if (sayac == 2)
+            {
+                Console.WriteLine("Girdiğiniz {0} sayısı Asal Sayıdır.", sayi);
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz {0} sayısı Asal Sayı Değildir.", sayi);
+            }
+        }
+    }
+}
